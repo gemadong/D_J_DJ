9bea0832370c2906e2d14e1d3a65c40c6f6fc149
Jinwoo28 <91857768+Jinwoo28@users.noreply.github.com>
2021-10-24 23:41:37 +0900

    20211024 수정중44

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -15,16 +15,6 @@ public class GameManager : MonoBehaviour
     public float DayTime = 5f;
     private int stage=0;
 
-    //public bool isBattle;
-    //public float Zrange;
-    //public float Xrange;
-    //public int CntN;
-    //public int CntS;
-    //public int CntT;
-    //public int CntJ;
-    //public int CntM;
-    //public int CntBos;
-
     /// <summary>
     /// //////////////////////////////////////
     /// </summary>
@@ -51,7 +41,6 @@ public class GameManager : MonoBehaviour
         else instance = this;
 
         zombieList = new List<int>();
-        //ZombieSpawnManager.GetComponent<ZombieSpawnManager>().ZombieSpawnStart();
 
     }
 
@@ -82,113 +71,6 @@ public class GameManager : MonoBehaviour
             DayTime = 5f;
         }
     }
-
-    //public void StageStart()
-    //{
-    //    StartCoroutine(InBattle());
-    //}
-    //public void StageEnd()
-    //{
-    //    Day();
-    //    stage++;
-    //}
-    //IEnumerator InBattle()
-    //{
-    //    if (stage % 3 == 0)
-    //    {
-    //        CntBos++;
-    //        int ranZone = Random.Range(0, 4);
-    //        if (ranZone < 2)
-    //        {
-    //            Debug.Log("社発");
-    //            if (ranZone == 0) Xrange = 50f;
-    //            else Xrange = -50f;
-    //            GameObject Bos = Instantiate(boss[0]);
-    //            Bos.GetComponent<Bos_1>().target = instance.transform;
-    //            float posZ = Random.Range(-50f, 50f);
-    //            Vector3 pos = new Vector3(Xrange, 0f, posZ);
-    //            Bos.transform.position = pos;
-    //            yield return new WaitForSeconds(2f);
-    //        }
-    //        else
-    //        {
-    //            Debug.Log("社発");
-    //            if (ranZone == 2) Zrange = 50f;
-    //            else Zrange = -50f;
-    //            GameObject Bos = Instantiate(boss[0]);
-    //            Bos.GetComponent<Bos_1>().target = instance.transform;
-    //            float posX = Random.Range(-50f, 50f);
-    //            Vector3 pos = new Vector3(posX, 0f, Zrange);
-    //            Bos.transform.position = pos;
-    //            yield return new WaitForSeconds(2f);
-    //        }
-
-    //    }
-    //    else
-    //    {
-    //        for (int i = 0; i < stage*5; i++)
-    //        {
-    //            int ran = Random.Range(0, 5);
-    //            zombieList.Add(ran);
-
-    //            switch (ran)
-    //            {
-    //                case 0:
-    //                    CntN++;
-    //                    break;
-    //                case 1:
-    //                    CntS++;
-    //                    break;
-    //                case 2:
-    //                    CntT++;
-    //                    break;
-    //                case 3:
-    //                    CntJ++;
-    //                    break;
-    //                case 4:
-    //                    CntM++;
-    //                    break;
-    //            }
-    //        }
-
-    //        while (zombieList.Count > 0)
-    //        {
-    //            int ranZone = Random.Range(0, 4);
-    //            if (ranZone < 2)
-    //            {
-    //                //Debug.Log("社発");
-    //                if (ranZone == 0) Xrange = 50f;
-    //                else Xrange = -50f;
-    //                GameObject jombie = Instantiate(zombies[zombieList[0]]);
-    //                jombie.GetComponent<Jombie>().player = players[0];
-    //                float posZ = Random.Range(-50f, 50f);
-    //                Vector3 pos = new Vector3(Xrange, 0f, posZ);
-    //                jombie.transform.position = pos;
-    //                zombieList.RemoveAt(0);
-    //                yield return new WaitForSeconds(2f);
-    //            }
-    //            else
-    //            {
-    //                //Debug.Log("社発");
-    //                if (ranZone == 2) Zrange = 50f;
-    //                else Zrange = -50f;
-    //                GameObject jombie = Instantiate(zombies[zombieList[0]]);
-    //                jombie.GetComponent<Jombie>().player = players[0];
-    //                float posX = Random.Range(-50f, 50f);
-    //                Vector3 pos = new Vector3(posX, 0f, Zrange);
-    //                jombie.transform.position = pos;
-    //                zombieList.RemoveAt(0);
-    //                yield return new WaitForSeconds(2f);
-    //            }
-    //        }
-    //        while (CntN + CntS + CntT + CntJ + CntM + CntBos > 0)
-    //        {
-    //            yield return null;
-    //        }
-    //        yield return new WaitForSeconds(4f);
-    //        StageEnd();
-    //    }
-    //}
 
     /// ///////////////////////////////
 
