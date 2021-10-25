a333194a790bb61f20b74d4d54496abe70a67772
Jinwoo28 <91857768+Jinwoo28@users.noreply.github.com>
2021-10-25 16:48:30 +0900

    00

--- Assets/Script/Enomy/Bos_2.cs
+++ Assets/Script/Enomy/Bos_2.cs
@@ -2,67 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bos_2 : Jombie
+public class Bos_2 : MonoBehaviour
 {
     [SerializeField] private GameObject PoisonPrefab = null;
     float delayTime = 0;
-    private bool isAttack_1 = true;
-    public float A = 1.0f;
-    protected override void Awake()
-    {
-        base.Awake();
-        hp = 60;
-        attackDelay = 4f;
-        attackPower = 20;
-        sight = 5.6f;
-        atkRng = 5.5f;
+    float delayTime_A = 0;
+    public Transform target;
+    private float bosSpeed=5f;
+    bool isattack;
+    int hp = 100;
+    Animator bosani_2;
 
-    }
-    protected override void Update()
+    private void Start()
     {
-        Debug.Log(state);
-        FindClosestPlayer();
+        isattack = true;
+        bosani_2 = GetComponent<Animator>();
+    }
+    private void Update()
+    {
         delayTime += Time.deltaTime;
-
-        switch (state)
-        {
-            case JombieState.Follow:
-                if (delayTime >= 5f)
-                {
-                    StartCoroutine("Fire");
-                    delayTime = 0;
-                    ZomAni.SetBool("isWalk", true);
-                }
-                else
-                {
-                    Follow();
-                }
-                break;
-            case JombieState.Attack:
-                if (isAttack)
-                {
-                    StartCoroutine("Attack_1");
-                    isAttack = false;    
-                }
-                break;
-            case JombieState.Die:
-                StartCoroutine("Die");
-                break;
-
-        }
+        delayTime_A += Time.deltaTime;
+        if(delayTime >= 5f) { Fire(); delayTime = 0; }
+        if (isattack == true) { RotateBos(); }
+        MoveBos();
     }
 
-    IEnumerator Fire()
+    public void Fire()
     {
-        ZomAni.SetBool("Att1",true);
-        yield return new WaitForSeconds(1f);
+        bosani_2.SetBool("Att1", true);
         GameObject poison = Instantiate(PoisonPrefab, transform.position, Quaternion.identity);
         poison.GetComponent<Poison>().Shoot(transform.forward);
-        ZomAni.SetBool("Att1", false);
+        bosani_2.SetBool("Att1", false);
     }
-    IEnumerator Attack_1()
+    void RotateBos()
     {
+        Vector3 dir = target.position - transform.position;
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime);
 
+<<<<<<< HEAD
             isAttack = false;
             if (Vector3.Distance(transform.position, player.transform.position) < sight)
             {
@@ -90,89 +67,44 @@ public class Bos_2 : Jombie
 
         //¾Æ´Ï¸é ´Ù½Ã ÇÃ·¹ÀÌ¾î¿¡°Ô ÀÌµ¿
         else
+=======
+    }
+    void MoveBos()
+    {
+        if ((target.position - transform.position).magnitude >= 10)
+>>>>>>> parent of d25f02d (ìˆ˜ì •ì¤‘)
         {
-            ZomAni.SetBool("Att2", false);
-            state = JombieState.Follow;
-            currentTime = 0;
-            isAttack_1 = true;
+            transform.Translate(Vector3.forward * bosSpeed * Time.deltaTime);
+            bosani_2.SetBool("Walk", true);
         }
+        if ((target.position - transform.position).magnitude < 10)
+        {
+            bosani_2.SetBool("Walk", false);
+            if (delayTime_A >= 5f) { StartCoroutine("Attack");; delayTime_A = 0; }
+        }
+    }
+    IEnumerator Attack()
+    {
+        isattack = false;
+        bosani_2.SetBool("Att2", true);
+        for (int i =-10;i<120; i+=3)
+        {
+            Vector3 roR = new Vector3(0, i, 0);
+            GameObject poison = Instantiate(PoisonPrefab, transform.position, Quaternion.identity);
+            Vector3 dir = transform.forward + roR*Time.deltaTime;
+            poison.GetComponent<Poison>().Shoot(dir);
+            yield return new WaitForSeconds(0.01f);
+        }
+        isattack = true;
+        bosani_2.SetBool("Att2", false);
+    }
+    public void Damage(int _dmg)
+    {
+        hp -= _dmg;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+            //if (onDeath != null) onDeath();
+        }
+    }
 }
-}
-
-
-//public class Bos_2 : MonoBehaviour
-//{
-//    [SerializeField] private GameObject PoisonPrefab = null;
-//    float delayTime = 0;
-//    float delayTime_A = 0;
-//    public Transform target;
-//    private float bosSpeed=5f;
-//    bool isattack;
-//    int hp = 100;
-//    Animator bosani_2;
-
-//    private void Start()
-//    {
-//        isattack = true;
-//        bosani_2 = GetComponent<Animator>();
-//    }
-//    private void Update()
-//    {
-//        delayTime += Time.deltaTime;
-//        delayTime_A += Time.deltaTime;
-//        if(delayTime >= 5f) { Fire(); delayTime = 0; }
-//        if (isattack == true) { RotateBos(); }
-//        MoveBos();
-//    }
-
-//    public void Fire()
-//    {
-//        bosani_2.SetBool("Att1", true);
-//        GameObject poison = Instantiate(PoisonPrefab, transform.position, Quaternion.identity);
-//        poison.GetComponent<Poison>().Shoot(transform.forward);
-//        bosani_2.SetBool("Att1", false);
-//    }
-//    void RotateBos()
-//    {
-//        Vector3 dir = target.position - transform.position;
-//        transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(dir), 5 * Time.deltaTime);
-
-//    }
-//    void MoveBos()
-//    {
-//        if ((target.position - transform.position).magnitude >= 10)
-//        {
-//            transform.Translate(Vector3.forward * bosSpeed * Time.deltaTime);
-//            bosani_2.SetBool("Walk", true);
-//        }
-//        if ((target.position - transform.position).magnitude < 10)
-//        {
-//            bosani_2.SetBool("Walk", false);
-//            if (delayTime_A >= 5f) { StartCoroutine("Attack");; delayTime_A = 0; }
-//        }
-//    }
-//    IEnumerator Attack()
-//    {
-//        isattack = false;
-//        bosani_2.SetBool("Att2", true);
-//        for (int i =-10;i<120; i+=3)
-//        {
-//            Vector3 roR = new Vector3(0, i, 0);
-//            GameObject poison = Instantiate(PoisonPrefab, transform.position, Quaternion.identity);
-//            Vector3 dir = transform.forward + roR*Time.deltaTime;
-//            poison.GetComponent<Poison>().Shoot(dir);
-//            yield return new WaitForSeconds(0.01f);
-//        }
-//        isattack = true;
-//        bosani_2.SetBool("Att2", false);
-//    }
-//    public void Damage(int _dmg)
-//    {
-//        hp -= _dmg;
-//        if (hp <= 0)
-//        {
-//            Destroy(gameObject);
-//            //if (onDeath != null) onDeath();
-//        }
-//    }
-//}
