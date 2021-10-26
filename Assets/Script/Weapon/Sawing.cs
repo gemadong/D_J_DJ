8c0b51c154c072a4ba6fec19adf2c7480d8479b1
Jinwoo28 <91857768+Jinwoo28@users.noreply.github.com>
2021-10-26 10:22:25 +0900

    2교시

--- Assets/Script/Weapon/Sawing.cs
+++ Assets/Script/Weapon/Sawing.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Sawing : Weapon
 {
-
+    [SerializeField] private ParticleSystem PS = null;
     private bool isAtk = false;
+    private bool isParticle = false;
+
         void Awake()
         {
             this.type = Type.Electric;
@@ -13,18 +15,21 @@ public class Sawing : Weapon
             AtkDelay = 1.0f;
             AtkRange = GetComponent<BoxCollider>();
             AtkRange.enabled = false;
+            PS.Stop();
+        
         }
     
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) PS.Play();
+        if (Input.GetMouseButtonUp(0)) PS.Stop();
     }
 
     public override void Attack()
     {
         StartCoroutine("SawingAtk");
-
     }
 
     IEnumerator SawingAtk()
